34fa8b9a2a74244d508b500250e9811eda0ba505
verissimohenry <verissimohenry04@gmail.com>
2021-08-06 13:34:01 +0100

    modify config file

--- js/game.js
+++ js/game.js
@@ -1,3 +1,19 @@
 var config = {
+    type: Phaser.WEBGL,
+    width: 480,
+    height: 640,
+    backgroundColor: "black",
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: [],
+    pixelArt: true,
+    roundPixels: true
 
 };
