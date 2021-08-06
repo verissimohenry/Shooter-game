a012628c7282c2a8558f2b7a5bb66dec7a7e2624
verissimohenry <verissimohenry04@gmail.com>
2021-08-06 16:24:41 +0100

    modify entities file

--- js/Entities.js
+++ js/Entities.js
@@ -1,6 +1,7 @@
 class Entity extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, key, type) {
-        super(scene, x, y, key);
+        super(scene, x, y, key, "Player");
+        this.setData("speed", 200);
         this.scene = scene;
         this.scene.add.existing(this);
         this.scene.physics.world.enableBody(this, 0);
