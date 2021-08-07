aa6e92f4fb0e2110ff196bccf6655a0b41f27dc2
verissimohenry <verissimohenry04@gmail.com>
2021-08-07 21:24:34 +0100

    add class enemylaser to entities file

--- js/Entities.js
+++ js/Entities.js
@@ -50,6 +50,13 @@ class Entity extends Phaser.GameObjects.Sprite {
                 this.play("sprEnemy2");
             }
         }
+
+        class EnemyLaser extends Entity {
+            constructor(scene, x, y) {
+                super(scene, x, y, "sprLaserEnemy0");
+                this.body.velocity.y = 200;
+            }
+        }
     }
 
 
