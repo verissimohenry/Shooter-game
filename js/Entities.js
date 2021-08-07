776f2e4af8191ce2c2d219fe5adc3935b3c18855
verissimohenry <verissimohenry04@gmail.com>
2021-08-07 21:59:01 +0100

    modify entities

--- js/Entities.js
+++ js/Entities.js
@@ -39,7 +39,29 @@ class Entity extends Phaser.GameObjects.Sprite {
             constructor(scene, x, y) {
                 super(scene, x, y, "sprEnemy0", "GunShip");
                 this.body.velocity.y = Phaser.Math.Between(50, 100);
+                this.shootTimer = this.scene.time.addEvent({
+                    delay: 1000,
+                    callback: function () {
+                        var laser = new EnemyLaser(
+                            this.scene,
+                            this.x,
+                            this.y
+                        );
+                        laser.setScale(this.scaleX);
+                        this.scene.enemyLasers.add(laser);
+                    },
+                    callbackScope: this,
+                    loop: true
+                });
                 this.play("sprEnemy0");
+            }
+
+            onDestroy () {
+                if (this.shootTimer !== undefined) {
+                    if (this.shootTimer) {
+                        this.shootTimer.remove(false);
+                    }
+                }
             }
         }
 
@@ -58,6 +80,4 @@ class Entity extends Phaser.GameObjects.Sprite {
             }
         }
     }
-
-
 }
