4d014aa15a8cd318e23ee681a4c45d9c6d2e95ae
verissimohenry <verissimohenry04@gmail.com>
2021-08-09 14:14:59 +0100

    Modify scenegameover file

--- js/SceneGameOver.js
+++ js/SceneGameOver.js
@@ -5,5 +5,58 @@ class SceneGameOver extends Phaser.Scene {
     });
   }
 
-  create = () => {};
+  create = () => {
+    this.sfx = {
+      btnOver: this.sound.add("sndBtnOver"),
+      btnDown: this.sound.add("sndBtnDown"),
+    };
+
+    this.title = this.add.text(this.game.config.width * 0.5, 128, "GAME OVER", {
+      fontFamily: "monospace",
+      fontSize: 48,
+      fontStyle: "bold",
+      color: "#ffffff",
+      align: "center",
+    });
+    this.title.setOrigin(0.5);
+
+    this.btnRestart = this.add.sprite(
+      this.game.config.width * 0.5,
+      this.game.config.height * 0.5,
+      "sprBtnRestart"
+    );
+
+    this.btnRestart.setInteractive();
+
+    this.btnRestart.on(
+      "pointerover",
+      function () {
+        this.btnRestart.setTexture("sprBtnRestartHover"); // set the button texture to sprBtnPlayHover
+        this.sfx.btnOver.play(); // play the button over sound
+      },
+      this
+    );
+
+    this.btnRestart.on("pointerout", function () {
+      this.setTexture("sprBtnRestart");
+    });
+
+    this.btnRestart.on(
+      "pointerdown",
+      function () {
+        this.btnRestart.setTexture("sprBtnRestartDown");
+        this.sfx.btnDown.play();
+      },
+      this
+    );
+
+    this.btnRestart.on(
+      "pointerup",
+      function () {
+        this.btnRestart.setTexture("sprBtnRestart");
+        this.scene.start("SceneMain");
+      },
+      this
+    );
+  };
 }
