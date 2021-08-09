dd9f291567d73a19b52740b7ac486ddb519351d6
verissimohenry <verissimohenry04@gmail.com>
2021-08-09 14:16:19 +0100

    Modify Scene Gameover

--- js/SceneGameOver.js
+++ js/SceneGameOver.js
@@ -58,5 +58,13 @@ class SceneGameOver extends Phaser.Scene {
       },
       this
     );
+
+    this.backgrounds = [];
+    for (var i = 0; i < 5; i++) {
+      var keys = ["sprBg0", "sprBg1"];
+      var key = keys[Phaser.Math.Between(0, keys.length - 1)];
+      var bg = new ScrollingBackground(this, key, i * 10);
+      this.backgrounds.push(bg);
+    }
   };
 }
