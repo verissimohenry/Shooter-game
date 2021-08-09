fada1f3c70ef21e271de18613d5d5716610a9eda
verissimohenry <verissimohenry04@gmail.com>
2021-08-09 09:40:24 +0100

    modify entities.js file

--- js/Entities.js
+++ js/Entities.js
@@ -161,8 +161,24 @@ explode = () => {
 }
 
 class ScrollingBackground {
-    constructor(scene, key, velocityY) {
-
+    constructor(_scene, _key, _velocityY) {
+        this.layers = this.scene.add.group();
     }
 }
 
+createLayers = () => {
+    for (var i = 0; i < 2; i++) {
+        // creating two backgrounds will allow a continuous scroll
+        var layer = this.scene.add.sprite(0, 0, this.key);
+        layer.y = (layer.displayHeight * i);
+        var flipX = Phaser.Math.Between(0, 10) >= 5 ? -1 : 1;
+        var flipY = Phaser.Math.Between(0, 10) >= 5 ? -1 : 1;
+        layer.setScale(flipX * 2, flipY * 2);
+        layer.setDepth(-5 - (i - 1));
+        this.scene.physics.world.enableBody(layer, 0);
+        layer.body.velocity.y = this.velocityY;
+
+        this.layers.add(layer);
+    }
+    this.createLayers();
+}
